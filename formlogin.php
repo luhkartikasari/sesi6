ac684cf2c38818cf47d73e3eb4caf7b526408159
luhkartikasari <127809285+luhkartikasari@users.noreply.github.com>
2023-04-18 12:12:57 +0800

    Add files via upload

---
+++ formlogin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+<form action="formlogin.php" method="POST">
+
+    <div>
+        Username
+        <input type="text" name="txUSER">
+    </div>
+
+    <div>
+        Password
+        <input type="text" name="txPASS1">
+    </div>
+
+    <div>
+    <button type="submit">Login</button>  
+    <button type="submit">Registrasi</button><br>
+    </div>
+    
+</body>
+</html>
